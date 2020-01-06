80977651a3f4a8dea9ec6bdb75d5b0b40ff0e166
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-01-06 15:50:11 +0000

    Update 101-Create networks.ps1

--- 101-Create networks.ps1
+++ 101-Create networks.ps1
@@ -1,10 +1,8 @@
-﻿#Login to customer tenant
-Connect-AzAccount
-
+﻿#Set Varriables
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 
-
+#Build Resources
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
