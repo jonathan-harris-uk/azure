7b8dd88aae0bfe661f51efdaf2eb8c0d642b6651
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-01-06 15:52:19 +0000

    Update 101-Create networks.ps1

--- 101-Create networks.ps1
+++ 101-Create networks.ps1
@@ -6,6 +6,6 @@ $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
-    -TemplateUri "https://raw.githubusercontent.com/jonathan-harris-uk/azure/master/azuredeploy.json"
+    -TemplateUri "https://raw.githubusercontent.com/jonathan-harris-uk/azure/network/create-network.json"
 
  (Get-AzVm -ResourceGroupName $resourceGroupName).name
