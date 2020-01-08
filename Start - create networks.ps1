3f21fd6fd63921fc4162a11a8f4b474b9252380a
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-01-08 14:34:15 +0000

    Update Start - create networks.ps1

--- Start - create networks.ps1
+++ Start - create networks.ps1
@@ -1,6 +1,7 @@
 ﻿#Set Varriables
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$publicIP = Read-Host -Prompt "Enter the customer's Public IP Address - used to lock down RDP access"
 
 #Build Resources
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
