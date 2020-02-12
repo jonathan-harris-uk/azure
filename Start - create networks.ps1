8417ec1b42773060b811c0f49eec491745e28f75
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-02-12 12:04:19 +0000

    Update Start - create networks.ps1

--- Start - create networks.ps1
+++ Start - create networks.ps1
@@ -1,6 +1,6 @@
 ﻿#Set Varriables
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$location = Read-Host -Prompt "Enter the location (i.e. uksouth)"
 $publicIP = Read-Host -Prompt "Enter the customer's Public IP Address - used to lock down RDP access"
 
 #Build Resources
