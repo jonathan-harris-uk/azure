abfb9e4d57dc718862e593f86d8b837001cf7d1f
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-03-03 17:37:47 +0000

    Update 101-VM-Operator-Role.ps1

--- 101-VM-Operator-Role.ps1
+++ 101-VM-Operator-Role.ps1
@@ -2,6 +2,6 @@
 $subscriptionId = Read-Host -Prompt "Enter Azure subscription ID, from Get-AzSubscription command"
 
 #Build Resources
-New-AzResourceGroupDeployment `
+az role definition create --role-definition `
     -TemplateUri "https://raw.githubusercontent.com/jonathan-harris-uk/azure/roles/VM-Operator-Role.json" `
     -subscriptionId $subscriptionId
