3b65f1e20242119432ebc1a97f4d0084df801066
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-03-27 18:05:17 +0000

    Update 101 start - create-vm.ps1

--- 101 start - create-vm.ps1
+++ 101 start - create-vm.ps1
@@ -1,14 +1,14 @@
 #Set Varriables
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$vmName = Read-Host -Prompt "Enter the VM Name"
-$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
-$vmSize = Read-Host -Prompt "Enter the VM Size"
+$subscriptionId = Read-Host -Prompt "Enter Azure subscription ID, from Get-AzSubscription command"
+$resourceGroupName = "vm-rg"
+$location = Read-Host -Prompt "Enter the location (i.e. uksouth, northeurope)"
+$vmName = Read-Host -Prompt "Enter the VM Name (i.e. aznevm1)"
+$dnsLabelPrefix = $vmName
+$vmSize = Read-Host -Prompt "Enter the VM Size (i.e. Standard_B2s, Standard_B4ms, Standard_B8ms)"
 $adminUsername = Read-Host -Prompt "Enter the administrator username"
 $adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
-$windowsOSVersion = Read-Host -Prompt "Enter the Windows version for the VM"
-$osDiskSize = Read-Host -Prompt "Enter the OS disk size"
-$subscriptionId = Read-Host -Prompt "Enter Azure subscription ID, from Get-AzSubscription command"
+$windowsOSVersion = Read-Host -Prompt "Enter the Windows version for the VM (i.e. 2012-R2-Datacenter-smalldisk, 2016-Datacenter-smalldisk, 2012-R2-Datacenter, 2016-Datacenter, 2016-Datacenter)"
+$osDiskSize = Read-Host -Prompt "Enter the OS disk size (i.e. 64, 128, 256, 512, 1024, 2048)"
 
 #Build Resources
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
