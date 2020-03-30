ea372860d5ef2b649688709b8be2157c0fc31043
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-03-30 19:06:04 +0100

    Update 101-VM-Operator-Role.ps1

--- 101-VM-Operator-Role.ps1
+++ 101-VM-Operator-Role.ps1
@@ -4,7 +4,16 @@ $subscriptions = Get-AzSubscription
 #Set Variables
 $subscriptionId = $subscriptions.id
 
-#Build Resources
-az role definition create --role-definition `
-    -TemplateUri "https://raw.githubusercontent.com/jonathan-harris-uk/azure/roles/VirtualMachineOperatorRole.json" `
-    -subscriptionId $subscriptionId
+$role = Get-AzRoleDefinition -Name "Virtual Machine Contributor"
+$role.Id = $null
+$role.Name = "Virtual Machine Operator"
+$role.Description = "Can deallocate, start  and restart virtual machines."
+$role.Actions.RemoveRange(0,$role.Actions.Count)
+$role.Actions.Add("Microsoft.Compute/*/read")
+$role.Actions.Add("Microsoft.Compute/virtualMachines/start/action")
+$role.Actions.Add("Microsoft.Compute/virtualMachines/restart/action")
+$role.Actions.Add("Microsoft.Compute/virtualMachines/deallocate/action")
+$role.AssignableScopes.Clear()
+$role.AssignableScopes.Add("/subscriptions/$subscriptionid")
+
+New-AzRoleDefinition -Role $role
