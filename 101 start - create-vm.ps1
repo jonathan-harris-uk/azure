6d3164cb62f1b4a9c6e5e22761fa8211c3ab2ecf
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-03-31 16:48:33 +0100

    Update 101 start - create-vm.ps1

--- 101 start - create-vm.ps1
+++ 101 start - create-vm.ps1
@@ -11,7 +11,7 @@ $dnsLabelPrefix = $vmName
 $vmSize = Read-Host -Prompt "Enter the VM Size (i.e. Standard_B2s, Standard_B4ms, Standard_B8ms)"
 $adminUsername = Read-Host -Prompt "Enter the administrator username"
 $adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
-$windowsOSVersion = Read-Host -Prompt "Enter the Windows version for the VM (i.e. 2012-R2-Datacenter-smalldisk, 2016-Datacenter-smalldisk, 2012-R2-Datacenter, 2016-Datacenter, 2016-Datacenter)"
+$windowsOSVersion = Read-Host -Prompt "Enter the Windows version for the VM (i.e. 2012-R2-Datacenter-smalldisk, 2016-Datacenter-smalldisk, 2012-R2-Datacenter, 2016-Datacenter, 2019-Datacenter)"
 $osDiskSize = Read-Host -Prompt "Enter the OS disk size (i.e. 64, 128, 256, 512, 1024, 2048)"
 
 #Build Resources
