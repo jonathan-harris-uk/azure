1626b0657fdb3d2ac5d755fa5ed8514bc7f09e92
jonathan-harris-uk <51961360+jonathan-harris-uk@users.noreply.github.com>
2020-04-28 15:35:04 +0100

    Update Policy.ps1

--- Policy.ps1
+++ Policy.ps1
@@ -1,7 +1,7 @@
 $Subscription = Get-AzSubscription
 
 $Policy = Get-AzPolicyDefinition -BuiltIn | Where-Object {$_.Properties.DisplayName -eq 'Allowed locations'}
-New-AzPolicyAssignment -Name 'Restrict Location' -PolicyDefinition $Policy -Scope "/subscriptions/$($Subscription.Id)" -PolicyParameter .\AllowedLocations.json
+New-AzPolicyAssignment -Name 'Restrict Location' -PolicyDefinition $Policy -Scope "/subscriptions/$($Subscription.Id)" -PolicyParameter https://raw.githubusercontent.com/jonathan-harris-uk/azure/Policy/AllowedLocations.json
 
 $Policy2 = Get-AzPolicyDefinition -BuiltIn | Where-Object {$_.Properties.DisplayName -eq 'Allowed virtual machine SKUs'}
-New-AzPolicyAssignment -Name 'Restrict VM SKUs' -PolicyDefinition $Policy2 -Scope "/subscriptions/$($Subscription.Id)" -PolicyParameter .\AllowedVmSKUs.json
+New-AzPolicyAssignment -Name 'Restrict VM SKUs' -PolicyDefinition $Policy2 -Scope "/subscriptions/$($Subscription.Id)" -PolicyParameter https://raw.githubusercontent.com/jonathan-harris-uk/azure/Policy/AllowedVmSKUs.json
